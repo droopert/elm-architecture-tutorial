de2819afbe761b1612a67d2d588db17b466529f7
Andrew Barrette <barrette.andrew.dev@gmail.com>
2017-12-29 13:44:30 -0600

    example 5 - add dropdown selector for topic selection, add styling, change error messages and use them in main view

--- examples/05-http.elm
+++ examples/05-http.elm
@@ -23,7 +23,8 @@ main =
 type alias Model =
     { topic : String
     , gifUrl : String
-    , error : String
+    , error : Maybe Http.Error
+    , selectOptions : List String
     }
 
 
@@ -35,9 +36,23 @@ errorGif =
     "http://pa1.narvii.com/6508/c2dfff227b76d11810cf2aff2e5487ac17b05a3b_00.gif"
 
 
+selectOptions =
+    [ "cat"
+    , "dog"
+    , "bat"
+    , "jingle"
+    , "snowman"
+    , "bog"
+    , "hog"
+    , "smaug"
+    , "hitchhikers"
+    , "goal"
+    ]
+
+
 init : String -> ( Model, Cmd Msg )
 init topic =
-    ( Model topic loadingGif ""
+    ( Model topic loadingGif Nothing selectOptions
     , getRandomGif topic
     )
 
@@ -62,10 +77,10 @@ update msg model =
             init topic
 
         NewGif (Ok newUrl) ->
-            ( Model model.topic newUrl "", Cmd.none )
+            ( { model | gifUrl = newUrl, error = Nothing }, Cmd.none )
 
         NewGif (Err error) ->
-            ( { model | gifUrl = errorGif, error = toString error }, Cmd.none )
+            ( { model | gifUrl = errorGif, error = Just error }, Cmd.none )
 
 
 
@@ -74,42 +89,104 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ label [] [ text "Topic" ]
-        , input [ onInput TopicChange, placeholder "cats" ] []
-        , button [ onClick MorePlease ] [ text "Find Gif!" ]
+    div [ style styles.wrapper ]
+        [ input
+            [ style <| List.concat [ styles.select, styles.inputs ]
+            , onInput TopicChange
+            , placeholder "cats"
+            ]
+            []
+        , select
+            [ style <| List.concat [ styles.select, styles.inputs ]
+            , onInput TopicChange
+            ]
+          <|
+            List.map selectOption model.selectOptions
+        , button
+            [ style <| List.concat [ styles.inputs, styles.button ]
+            , onClick MorePlease
+            ]
+            [ text "Find Gif!" ]
         , br [] []
         , errorBlock model
-        , img [ src model.gifUrl ] []
+        , img [ style styles.img, src model.gifUrl ] []
         ]
 
 
+selectOption value =
+    option [] [ text value ]
+
+
 errorBlock model =
-    if String.isEmpty model.error then
-        text ""
-    else
-        div []
-            [ h3 [] [ text "Aw, crap--an error!" ]
-            , p [] [ text model.error ]
-            ]
+    case model.error of
+        Nothing ->
+            text ""
+
+        Just error ->
+            div [ style styles.errorWrapper ]
+                [ h1 [] [ text "Shit! Something screwy happened..." ]
+                , p [ style styles.errorMessage ] [ text <| errorMessage error ]
+                , sub [] [ text <| toString error ]
+                ]
 
 
 errorMessage error =
     case error of
         Http.BadUrl error ->
-            "The image URL is bogus, man"
+            "The image URL Giphy gave us is bullshit, man."
 
         Http.Timeout ->
-            "The internets are wasting our time - we gave up"
+            "The internets were wasting our time, so we gave up."
 
         Http.NetworkError ->
-            "The network is very grumpy"
+            "The network is being a dick right now. Trying again will probably work."
 
         Http.BadStatus response ->
-            "The image status is effed"
+            "Giphy is effed. Screw them anyway."
 
         Http.BadPayload error response ->
-            "We asked for an image and got something else, wtf"
+            "We asked Giphy for an image. They gave us something else. WTF?"
+
+
+styles =
+    { wrapper =
+        [ ( "text-align", "center" )
+        , ( "font", "1em/.8em monospace" )
+        , ( "background", "black" )
+        , ( "width", "100vw" )
+        , ( "height", "100vh" )
+        , ( "padding", "30px 15px" )
+        , ( "color", "coral" )
+        ]
+    , img =
+        [ ( "border", "3px solid white" )
+        , ( "margin", "30px" )
+        ]
+    , inputs =
+        [ ( "display", "block" )
+        , ( "margin", "15px auto" )
+        , ( "width", "250px" )
+        , ( "box-sizing", "border-box" )
+        ]
+    , select =
+        [ ( "height", "2.5em" ) ]
+    , button =
+        [ ( "background", "darkgray" )
+        , ( "padding", "10px" )
+        , ( "font", "inherit" )
+        , ( "border-radius", "3px" )
+        , ( "border", "1px solid white" )
+        ]
+    , errorWrapper =
+        [ ( "max-width", "600px" )
+        , ( "margin", "0 auto" )
+        ]
+    , errorMessage =
+        [ ( "font-size", "1.25em" )
+        , ( "line-height", "1.25em" )
+        , ( "margin", "2.5em" )
+        ]
+    }
 
 
 
